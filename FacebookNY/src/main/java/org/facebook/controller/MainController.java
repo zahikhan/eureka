ba48e632b6a9bb7b27c035e0e207e21c9405033a
Zahid_Khan <xahiidkhan@gmail.com>
2023-10-06 09:27:49 +0530

    Modify first module.

---
+++ FacebookNY/src/main/java/org/facebook/controller/MainController.java
@@ -0,0 +1,14 @@
+package org.facebook.controller;
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+  @GetMapping
+  ResponseEntity<String> helloWorld() {
+    return new ResponseEntity<>("<h1>Facebook</h1>", HttpStatusCode.valueOf(200));
+  }
+}
